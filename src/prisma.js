2fe3f7bae6cdb6d9a94537c6b200bf486164e44e
Joon <joonseo.an@outlook.com>
2019-08-01 21:11:07 -0400

    updated secret key

--- src/prisma.js
+++ src/prisma.js
@@ -5,7 +5,7 @@ const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     // endpoint: 'http://192.168.99.100:4466',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: 'mysupersecrettext',
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements
 })
 
